1f180bb88ef4040987a8b3e12369cba942eaee9c
JiazeWang <jiazewang@csu.edu.cn>
2019-11-22 14:50:50 +0800

    1

--- torchreid/metrics/rank.py
+++ torchreid/metrics/rank.py
@@ -102,8 +102,8 @@ def eval_market1501(distmat, q_pids, g_pids, q_camids, g_camids, max_rank):
         print('Note: number of gallery samples is quite small, got {}'.format(num_g))
 
     indices = np.argsort(distmat, axis=1)
-
-    np.save("indices.npy", indices)
+    np.save("distance.npy")
+    #np.save("indices.npy", indices)
 
     matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
 
