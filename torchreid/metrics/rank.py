bca37f17713764a154f05f0f15f76646910162d3
JiazeWang <jiazewang@csu.edu.cn>
2019-11-22 14:45:42 +0800

    1

--- torchreid/metrics/rank.py
+++ torchreid/metrics/rank.py
@@ -102,6 +102,19 @@ def eval_market1501(distmat, q_pids, g_pids, q_camids, g_camids, max_rank):
         print('Note: number of gallery samples is quite small, got {}'.format(num_g))
 
     indices = np.argsort(distmat, axis=1)
+
+    max_200_indices = indices[:, :200]
+
+    res_dict = dict()
+    for q_idx in range(num_q):
+        print(q_pids[q_idx])
+        filename = q_pids[q_idx][q_pids[q_idx].rindex("\\")+1:]
+        max_200_files = [g_pids[i][g_pids[i].rindex("\\")+1:] for i in max_200_indices[q_idx]]
+        res_dict[filename] = max_200_files
+
+    with open(r'submission_A.json', 'w' ,encoding='utf-8') as f: # 提交文件
+        json.dump(res_dict, f)
+
     matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
 
     # compute cmc curve for each query
