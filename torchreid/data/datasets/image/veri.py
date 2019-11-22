bcbe9c391c955fa8cf3d01e1e90a1d92c3baf3fd
JiazeWang <jiazewang@csu.edu.cn>
2019-11-22 14:31:18 +0800

    1

--- torchreid/data/datasets/image/veri.py
+++ torchreid/data/datasets/image/veri.py
@@ -7,7 +7,9 @@ import re
 import os.path as osp
 
 from .base import BaseImageDataset
-
+from collections import defaultdict
+from torchreid.data.datasets import ImageDataset
+from torchreid.utils import read_json, write_json
 
 class VeRi(ImageDataset):
     """
